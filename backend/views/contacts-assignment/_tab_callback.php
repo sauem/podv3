bbf056209b09d08c04ca3bf28d52cf8ecae259c9
sauem <nguyendinhthang.go97@gmail.com>
2020-07-22 21:19:08 +0700

    update auto scan

--- backend/views/contacts-assignment/_tab_callback.php
+++ backend/views/contacts-assignment/_tab_callback.php
@@ -43,6 +43,9 @@ use yii\helpers\Url;
                 'attribute' => 'status',
                 'format' => 'html',
                 'value' => function ($model) {
+                    if(!$model->assignment){
+                        return  null;
+                    }
                     return $model->assignment->user->username;
                 }
             ],
--- backend/views/contacts/_tab_callback.php
+++ backend/views/contacts/_tab_callback.php
@@ -33,6 +33,9 @@ use common\helper\Helper;
                     'attribute' => 'category_id',
                     'format' => 'html',
                     'value' => function ($model) {
+                        if(!$model->page){
+                            return  null;
+                        }
                         return Html::tag("p",
                             $model->page->product->name . "<br><small>{$model->page->product->sku} | {$model->page->product->regular_price}</small><br>" .
                             "<small><i>{$model->page->category->name}</i></small>");
@@ -43,6 +46,9 @@ use common\helper\Helper;
                     'attribute' => 'link',
                     'format' => 'raw',
                     'value' => function ($model) {
+                        if(!$model->page){
+                            return  null;
+                        }
                         return Html::tag("p",
                             "<a target='_blank' href='{$model->link}' >{$model->page->link}  <i class='fa fa-chrome'></i></a><br><small>{$model->option}</small><br>" .
                             "<small class='text-danger'>Note: <i>{$model->note}</i></small>");
