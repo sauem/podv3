b4568800b45aec1be105aa1f2015b7d0b9e5b171
sauem <nguyendinhthang.go97@gmail.com>
2020-08-06 10:45:49 +0700

    update import and contact code

--- backend/views/contacts/_contact_histories.php
+++ backend/views/contacts/_contact_histories.php
@@ -4,6 +4,7 @@ use kartik\grid\GridView;
 use \kartik\grid\SerialColumn;
 use yii\helpers\Html;
 use backend\models\ContactsModel;
+use common\helper\Helper;
 
 ?>
 <div class="ibox">
@@ -27,13 +28,56 @@ use backend\models\ContactsModel;
             ],
             'columns' => [
                 [
-                    'label' => 'Contact',
+                    'label' => 'Số điện thoại',
+                    'attribute' => 'created_at',
+                    'format' => 'html',
+                    'value' => function ($model) {
+                        return $model->contact->phone;
+                    }
+                ],
+                [
+                    'label' => 'Sản phẩm',
                     'attribute' => 'contact_id',
+                    'headerOptions' => ['width' => '30%'],
                     'format' => 'raw',
                     'value' => function ($model) {
-                        $html =  $model->contact->code."<br>";
-                        $html .= "<small class='text-warning'>{$model->created_at}</small>";
+                        if (!$model->contact->page || !$model->contact->page->product) {
+                            return null;
+                        }
+                        $page = $model->contact->page;
+                        $html = $page->product->name . "<br>";
+                        $html .= "<small>{$page->product->sku}</small>|";
+                        $html .= "<small>" . Helper::money($page->product->regular_price) . "</small>|";
+                        $html .= "<small>{$page->product->category->name}</small><br>";
+                        $html .= "<small>{$model->contact->option}</small>";
+
                         return $html;
+                    }
+                ],
+                [
+                    'label' => 'Trang đích',
+                    'format' => 'html',
+                    'headerOptions' => ['width' => '30%'],
+                    'value' => function ($model) {
+                        if (!$model->contact->page) {
+                            return null;
+                        }
+                        return Html::tag("p",
+                            "<a target='_blank' href='{$model->contact->page->link}' >{$model->contact->page->link}  <i class='fa fa-chrome'></i></a><br>" .
+                            "<small class='text-info'>CTCODE: <i><strong>{$model->contact->code}</strong></i> | Marketer: <strong>{$model->contact->page->user->username}</strong></small><br>" .
+                            "<small class='text-info'>address: <i>{$model->contact->address}</i></small><br>" .
+                            "<small class='text-info'>zipcode: <i>{$model->contact->zipcode}</i></small><br>" .
+                            "<small class='text-danger'>Note: <i>{$model->contact->note}</i></small><br>"
+
+                        );
+                    }
+                ],
+                [
+                    'label' => 'Ngày liên hệ',
+                    'attribute' => 'created_at',
+                    'format' => 'html',
+                    'value' => function ($model) {
+                        return $model->created_at;
                     }
                 ],
                 [
