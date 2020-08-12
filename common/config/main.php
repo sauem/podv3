c7f0c460aeb65ffb211c01159f435821064cd25c
sauem <nguyendinhthang.go97@gmail.com>
2020-08-12 09:50:40 +0700

    update post

--- common/config/main.php
+++ common/config/main.php
@@ -44,7 +44,7 @@ return [
             'class' => 'yii\redis\Connection',
             'hostname' => REDIS_HOST,
             'port' => REDIS_PORT,
-            //'password' => REDIS_PASS,
+            'password' => REDIS_PASS,
             'database' => 0,
             'retries' => 1,
         ],
