5d5c86ad3f0d4bb1314c7b4f93fed749ab595f58
sauem <nguyendinhthang.go97@gmail.com>
2020-08-26 11:10:13 +0700

    update add munual order

--- backend/views/products/index.php
+++ backend/views/products/index.php
@@ -114,7 +114,7 @@ $this->params['breadcrumbs'][] = $this->title;
 </div>
 
 <div class="modal fade" tabindex="-1" id="product-import" role="dialog">
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Nhập sản phẩm</h5>
